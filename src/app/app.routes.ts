6c8db6b4697906914c5280e2d12e4132e9d333fb
tst gps <Parinya.Phapha@gmail.com>
2024-10-29 10:05:27 +0700

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: MainComponent,
+            children:[
+                {path: '', loadComponent: () => import('./main/home/home.component').then(x => x.HomeComponent)},
+                // {path: 'about', loadComponent: () => import('./main/about/about.component').then(x => x.AboutComponent)},
+            ]
+    },
+    {
+        path: '**', component: MainComponent,
+    },
+];
---
+++ src/app/main/main.component.ts
@@ -0,0 +1,35 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [RouterOutlet, RouterModule, CommonModule],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss'
+})
+export class MainComponent {
+
+  public userName: any;
+
+  // toggleMenu(): void {
+  //   const x = this.elementRef.nativeElement.querySelector("#myTopnav");
+  //   if (x.classList.contains("menu")) {
+  //     x.classList.add("responsive");
+  //   } else {
+  //     x.classList.remove("responsive");
+  //   }
+  // }
+  isMenuOpen: boolean = false;
+
+  ngOnInit(): void {
+    // กำหนดค่าให้ isMenuOpen เป็น false เมื่อหน้าเว็บโหลดเสร็จสิ้น
+    this.isMenuOpen = false;
+  }
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+}
